fd8236271c2612845d308657ef3c3051d6e9b608
Paul <p.tustain@hotmail.co.uk>
2025-12-03 16:32:12 +0000

    updated part 1 to be better, completed part 2

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -4,43 +4,39 @@ fn main() {
     dbg!(output);
 }
 
-fn get_largest(details: &str) -> u64 {
-    if details == "" {
-        return 0;
-    }
-    let mut first: u64 = 0;
-    let mut second: u64 = 0;
-
-    let final_char = (&details[details.len() - 1..].to_string())
-        .parse::<u64>()
-        .unwrap();
-    let details = &details[..details.len() - 1];
-
-    for v in details.chars() {
-        let number = (v.to_string()).parse::<u64>().unwrap();
-        if number > first {
-            first = number;
-            second = 0;
-        } else {
-            if number > second {
-                second = number;
-            }
+fn find_largest(reduced_vec: &[u32]) -> Option<(usize, u32)> {
+    let biggest_value = reduced_vec.iter().max().unwrap();
+    for (n, v) in reduced_vec.iter().enumerate() {
+        if v == biggest_value {
+            return Some((n, *v));
         }
     }
+    None
+}
 
-    if final_char > second {
-        second = final_char;
-    }
+fn get_largest(bank_vec: Vec<u32>) -> u32 {
+    let idx: usize = 0;
+    let mut batteries_used: Vec<String> = Vec::new();
 
-    first * 10 + second
+    let (idx, value) = find_largest(&bank_vec[idx..bank_vec.len() - 1]).unwrap();
+    batteries_used.push(value.to_string());
+
+    let (_, value) = find_largest(&bank_vec[idx + 1..bank_vec.len()]).unwrap();
+    batteries_used.push(value.to_string());
+
+    batteries_used.join("").parse::<u32>().unwrap()
 }
 
 fn find_output(input: &str) -> String {
     let banks: Vec<&str> = input.split("\n").collect();
-    let mut count: u64 = 0;
+    let mut count: u32 = 0;
 
     for bank in banks {
-        count += get_largest(bank);
+        if bank == "" {
+            break;
+        }
+        let bank_vec: Vec<u32> = bank.chars().flat_map(|ch| ch.to_digit(10)).collect();
+        count += get_largest(bank_vec);
     }
 
     count.to_string()
@@ -87,5 +83,17 @@ mod tests {
         let result = find_output("818181911112111");
 
         assert_eq!(result, "92".to_string());
+    }
+
+    #[test]
+    fn test_largest_idx() {
+        let (idx, value) = find_largest(&Vec::from([9, 6, 5, 3, 7])).unwrap();
+        assert_eq!((0, 9), (idx, value));
+    }
+
+    #[test]
+    fn test_multi_largest() {
+        let (idx, value) = find_largest(&Vec::from([9, 6, 5, 3, 9, 7, 1, 4, 7, 9])).unwrap();
+        assert_eq!((0, 9), (idx, value));
     }
 }
